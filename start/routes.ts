641b8c1f2128a1571bcf7049a2f5b07c06938aeb
Dhimasekaputraa <dhimasekaputraa@gmail.com>
2025-11-20 21:28:51 +0700

    adding some pages

--- start/routes.ts
+++ start/routes.ts
@@ -9,4 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 
+// Landing Pages
 router.on('/').render('pages/landing_page/lp')
+router.on('/about').render('pages/landing_page/about_us')
+router.on('/contact').render('pages/landing_page/contact_us')
