c7e373d25cb0bf56bb546f13888fb5c70fe078b6
AdriLorenzo99 <adrilorenzoml@gmail.com>
2025-11-21 11:50:15 +0700

    fitur login

--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,25 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
+const AuthController = () => import('#controllers/auth_controller')
 
+// Landing Pages
 router.on('/').render('pages/landing_page/lp')
+router.on('/about').render('pages/landing_page/about_us')
+router.on('/contact').render('pages/landing_page/contact_us')
+
+// Authentication Routes
+router.group(() => {
+  router.get('/register', [AuthController, 'register']).as('auth.register.show')
+  router.post('/register', [AuthController, 'handleRegister']).as('auth.register.store')
+  
+  router.get('/login', [AuthController, 'login']).as('auth.login.show')
+  router.post('/login', [AuthController, 'handleLogin']).as('auth.login.store')
+  
+  router.post('/logout', [AuthController, 'logout']).as('auth.logout')
+}).middleware(async (ctx, next) => {
+  // Bypass middleware guest untuk logout
+  if (ctx.request.url().includes('logout')) {
+    return next()
+  }
+  const guest = await import('#middleware/guest_middleware')
+  return new guest.default().handle(ctx, next)
+})
