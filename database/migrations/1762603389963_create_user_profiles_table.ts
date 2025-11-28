c70aba692ee3f143b16e1070457be51442b02c2d
Nuggynih <nuggynugini@gmail.com>
2025-11-28 17:06:39 +0700

    test

--- database/migrations/1762603389963_create_user_profiles_table.ts
+++ database/migrations/1762603389963_create_user_profiles_table.ts
@@ -7,14 +7,10 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
       table.integer('user_id').unsigned().references('users.id').notNullable()
-
-      table.string('full_name').references('users.full_name')
       table.string('username').notNullable().defaultTo('')
-      table.string('email',254).references('users.email').unique().notNullable()
-
+      table.string('email').notNullable()
       table.string('profile_picture',512).nullable()
       table.text('bio').notNullable().defaultTo('')
-
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
     })
